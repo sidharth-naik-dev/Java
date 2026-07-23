0718df43d80a572e2790f970e0791d7c628b493f
Sidharth Naik <sidsidharth2002@gmail.com>
2026-07-23 23:21:37 +0530

    Added StringLearn2 file
    It contains more of the String operations around 12 operations like equals(), endsWith(), startsWith(), indexOf() etc.

---
+++ java-practice/strings/StringLearn2.java
@@ -0,0 +1,27 @@
+package strings;
+
+import java.lang.String;
+
+public class StringLearn2 {
+
+    static void main() {
+        String name = "Java Programming Language";
+        System.out.println(name);
+        System.out.println("9. " + name.contains("Programming"));
+        System.out.println("10. " + name.startsWith("Java"));
+        System.out.println("11. " + name.endsWith("Language"));
+        System.out.println("12. " + name.equals("Java Programming Language"));
+        System.out.println("13. " + name.equalsIgnoreCase("java programming language"));
+        System.out.println("14. " + name.indexOf("Programming"));
+        String text = "Java is powerful. Java is popular.";
+        System.out.println("15. " + text.lastIndexOf("Java"));
+        System.out.println("16. " + name.replace("Java", "Python"));
+        String digits = "A1B2C3D4";
+        System.out.println("17. " + digits.replaceAll("[0-9]", "*"));
+        String s1 = "";
+        System.out.println("18. " + s1.isEmpty());
+        System.out.println("19. " + "Hi ".repeat(5));
+        System.out.println("20. " + "Apple".compareTo("Banana"));
+
+    }
+}
