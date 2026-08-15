bbcb3e2d8c5a1cd04a4f2dc16f0233d3e6d7c480
Sidharth Naik <sidsidharth2002@gmail.com>
2026-08-15 11:15:21 +0530

    Update StarPatterns.java
    Added 3 more star patterns - Butterfly, Rhombus and Diamond

--- java-practice/logic-building/StarPatterns.java
+++ java-practice/logic-building/StarPatterns.java
@@ -64,14 +64,92 @@ public class StarPatterns {
             System.out.println();
         }
     }
+    public void butterfly(){
+        System.out.println("Butter fly");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n-i);
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n-i);
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public void rhombusShape(){
+        System.out.println("Rhombus Shape");
+        int space = 4;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 5; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            space--;
+        }
+    }
+
+    public void diamondShape() {
+        System.out.println("Diamond Shape"); // Uses double white spaces and a space after asterisk,
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= (i * 2 - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        n--;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j <= (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= (i * 2 - 1); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 
     static void main() {
         StarPatterns sp = new StarPatterns ();
-//        sp.rightAngleTriangleShape();
+        sp.rightAngleTriangleShape();
         sp.upsideDownRightAngleTriangleShape();
-//        sp.pyramidShape();
-//        sp.squareShape();
-//        sp.hollowSquareShape();
-//        sp.upsideDownPyramidShape();
+        sp.pyramidShape();
+        sp.upsideDownPyramidShape();
+        sp.squareShape();
+        sp.hollowSquareShape();
+        sp.butterfly();
+        sp.rhombusShape();
+        sp.diamondShape();
     }
 }
